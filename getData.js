8620f9b750c778ad13abb9be483faa2779fe8aab
biancardona <biancardona.rivera@gmail.com>
2021-05-10 16:18:31 -0500

    Parameterizyng search form

--- getData.js
+++ getData.js
@@ -1,9 +1,12 @@
 let key = "AIzaSyA5gbq4gQ83AEP633ET52IbloKJiKlOAks";       // API KEY
 let id = "9a352a89890a7adf4";                             // CSE ID
-let q = "cats";
+// let q = "cats";
 let siteSearch = "https://www.pharmacy.ca.gov/"          // QUERY
 
-
+window.onload = function () {
+    //Aquí referenciamos el botón que realizara la acción
+    document.querySelector('#search').addEventListener('click', triggersearch)
+}
 function hndlr(response) {
     console.log(response)               // a way to see your results
     for (var i = 0; i < response.items.length; i++) {
@@ -13,11 +16,13 @@ function hndlr(response) {
 
     }
 }
+const inputSearch = document.querySelector("#query")
+const results = document.querySelector("#content")
 
 function triggersearch() {
-    let query = document.getElementById("query").value;
+    const input = inputSearch.value;
     let JSElement = document.createElement('script');
-    JSElement.src = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${id}&siteSearch=${siteSearch}&q=${q}` + query + '&callback=hndlr';
+    JSElement.src = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${id}&siteSearch=${siteSearch}&q=${input}` + '&callback=hndlr';
     document.getElementsByTagName('head')[0].appendChild(JSElement);
 }
 
